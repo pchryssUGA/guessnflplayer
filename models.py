76b3d7db0341be0689e9be31030303db4e656b75
Philip <philipchryssochoos@gmail.com>
2023-07-10 23:46:50 -0400

    Add reports column to db

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from db import db
 
-
-class players(db.Model):
+class playersDB(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     team = db.Column(db.String(100))
@@ -9,13 +8,15 @@ class players(db.Model):
     url = db.Column(db.String(200))
     numG = db.Column(db.Integer)
     numC = db.Column(db.Integer)
+    numR = db.Column(db.Integer)
     
     
-    def __init__(self, name, team, year, url, numG, numC):
+    def __init__(self, name, team, year, url, numG, numC, numR):
         self.name = name
         self.team = team
         self.year = year
         self.url = url
         self.numG = numG
         self.numC = numC
+        self.numR = numR
     
